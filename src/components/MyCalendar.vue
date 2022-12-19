4314ce0574fdbcb55f6cce2df4b5f14dd6fe224a
Kiang Kuang <kiangkuang@gmail.com>
2022-12-19 11:37:40 +0800

    calendar

---
+++ src/components/MyCalendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script setup lang="ts">
+import '@fullcalendar/core/vdom';
+import FullCalendar from '@fullcalendar/vue3';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { CalendarOptions } from '@fullcalendar/core';
+
+const calendarOptions: CalendarOptions = {
+  plugins: [timeGridPlugin],
+  initialView: 'timeGridWeek',
+  height: 'calc(100vh - 50px - 16px - 16px)',
+  events: [],
+};
+
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,7 +25,7 @@
 
 <script setup lang="ts">
 import { GoogleLogin, googleSdkLoaded } from 'vue3-google-login';
-import { useScriptTag, until } from '@vueuse/core';
+import { useScriptTag, until, invoke } from '@vueuse/core';
 import { ref } from 'vue';
 
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
@@ -34,6 +34,7 @@ const API_KEY = 'AIzaSyC4YLLkJAGPP1wmyiqzmkkaDzgZFwfPO1w';
 const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
 const gapiLoaded = ref(false);
+const gsiLoaded = ref(false);
 
 useScriptTag('https://apis.google.com/js/api.js', () => {
   window.gapi.load('client', async () => {
@@ -46,8 +47,6 @@ useScriptTag('https://apis.google.com/js/api.js', () => {
 });
 
 const getEvents = async () => {
-  await until(gapiLoaded).toBeTruthy();
-
   const response = await window.gapi.client.calendar.events.list({
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
@@ -56,17 +55,39 @@ const getEvents = async () => {
     maxResults: 10,
     orderBy: 'startTime',
   });
-  console.log(response);
+
+  return response.result.items.map((x) => ({
+    title: x.summary,
+    start: x.start.dateTime ?? x.start.date,
+    end: x.end.dateTime ?? x.end.date,
+  }));
 };
 
+let tokenClient: any;
 googleSdkLoaded(async (google) => {
-  google.accounts.oauth2
+  tokenClient = google.accounts.oauth2
     .initTokenClient({
       client_id: CLIENT_ID,
       scope: SCOPES,
       callback: getEvents,
-    })
-    .requestAccessToken();
+    });
+  gsiLoaded.value = true;
+});
+
+invoke(async () => {
+  await until(gsiLoaded).toBeTruthy();
+  await until(gapiLoaded).toBeTruthy();
+
+  const a = window.gapi.client.getToken();
+  console.log(a);
+  if (a === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: 'consent' });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: '' });
+  }
 });
 
 </script>
