ce9ddf60d3f5f2a62193513a48981cfc3484d276
Kiang Kuang <kiangkuang@gmail.com>
2022-12-20 03:30:48 +0800

    hide tooltip after sign in

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,7 +17,7 @@
         >
           <template v-slot:loading>
             <q-spinner/>
-            <q-tooltip anchor="center left" self="center right" class="text-body2 bg-dark">
+            <q-tooltip v-if="!isAuthenticated" anchor="center left" self="center right" class="text-body2 bg-dark">
               Sign in with Google!
             </q-tooltip>
           </template>
