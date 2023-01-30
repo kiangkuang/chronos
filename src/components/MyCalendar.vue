f213241442b5da4f8547ee043edb87e931927824
Kiang Kuang <kiangkuang@gmail.com>
2023-01-30 11:27:28 +0800

    handle add full day event

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -67,11 +67,14 @@ const calendarOptions = computed<CalendarOptions>(() => ({
     // eslint-disable-next-line no-alert
     const title = prompt('Add event', 'Name');
     if (title) {
+      const start = DateTime.fromJSDate(e.start);
+      const end = DateTime.fromJSDate(e.end);
       fullCalendar.value?.getApi().addEvent({
         id: crypto.randomUUID(),
-        start: DateTime.fromJSDate(e.start).toISO(),
-        end: DateTime.fromJSDate(e.end).toISO(),
+        start: start.toISO(),
+        end: end.toISO(),
         title,
+        allDay: start.startOf('day').equals(start) && end.startOf('day').equals(end),
       });
     }
     fullCalendar.value?.getApi().unselect();
