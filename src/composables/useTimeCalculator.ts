1875e07406fc2546dc5ee0763c3ea88cf2571c4b
Yuehu Lin <dev@fntsr.tw>
2023-03-18 04:06:02 +0800

    doc(time): calculate rule

--- src/composables/useTimeCalculator.ts
+++ src/composables/useTimeCalculator.ts
@@ -13,6 +13,16 @@ const {
   intervalsToHours,
 } = useTimeUtilities();
 
+// There have serveral events:
+// Basic
+//   (1) leave (whole day)
+//   (2) support (whole day)
+//   (3) meeting
+//   (4) improve (whole day)
+// Category
+//   (A) normal = meeting + leave + support
+// When events are overlap, will follow the priority:
+//   (1) > (2) > (3) > (4)
 const eventsIntervals = computed(() => selectedEvents.value.map((event) => createEventInterval(event)));
 const devTimeIntervals = computed(() => calcIntervalsDifference(workTimeIntervals.value, eventsIntervals.value));
 
