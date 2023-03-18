5dbf87435fa40f90bd17764f94a1f7d5888b9f48
Yuehu Lin <dev@fntsr.tw>
2023-03-19 05:05:46 +0800

    feat(toolbar): remove other setting

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,16 +19,6 @@
         <q-btn
           flat
           round
-          icon="settings"
-        >
-          <q-tooltip anchor="bottom middle" self="top middle" class="text-body2 bg-dark">
-            Other Setting
-          </q-tooltip>
-          <OtherSetting/>
-        </q-btn>
-        <q-btn
-          flat
-          round
           icon="disabled_visible"
           :text-color="showDeclinedEvent ? '' : 'grey'"
           :disable="isLoading"
@@ -115,15 +105,13 @@ import { useGoogle } from 'src/composables/useGoogle';
 import { useGoogleCalendar } from 'src/composables/useGoogleCalendar';
 import DateSettings from 'src/components/DateSettings.vue';
 import WorkingHourSetting from 'src/components/WorkingHourSetting.vue';
-import OtherSetting from 'src/components/OtherSetting.vue';
 import SendData from 'src/components/SendData.vue';
 import ExportText from 'src/components/ExportText.vue';
 
-const { avatarUrl, isForecast, showDeclinedEvent } = storeToRefs(useSettingsStore());
+const { avatarUrl, showDeclinedEvent } = storeToRefs(useSettingsStore());
 const { isLoading, isAuthenticated } = useGoogle();
 const { updateEvents, updateEventsIfAuthed } = useGoogleCalendar();
 
-const toggleRecordType = () => { isForecast.value = !isForecast.value; };
 const toggleShowDeclined = () => {
   showDeclinedEvent.value = !showDeclinedEvent.value;
   updateEventsIfAuthed();
