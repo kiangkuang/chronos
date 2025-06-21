6d8c99a95da8f995c8e1cfffeb4123d793595aff
Kiang Kuang <kiangkuang@gmail.com>
2025-06-22 00:45:54 +0800

    Rename CalendarPage component

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/HomePage.vue') },
-      { path: 'calendar', component: () => import('pages/IndexPage.vue') },
+      { path: 'calendar', component: () => import('pages/CalendarPage.vue') },
     ],
   },
 
